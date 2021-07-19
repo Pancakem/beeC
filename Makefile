a885d297280e1d8b2f032b24cb75106e69a8dd79
Pancakem <pancakesdeath@protonmail.com>
2021-07-20 01:57:26 +0300

    Prepare main

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
-CC = clang
+CC = gcc
 files = main.c parser.c type.c tokenizer.c codegen.c
 incs = parser.h type.h tokenizer.h codege.h 
 program = compiler
 
 build:
-	$(CC) -I incs $(files) -o $(program) -O3 -Wall -pedantic -v
+	$(CC) -I incs $(files) -o $(program) -O3 -Wall -pedantic
 
 clean:
 	rm $(program) *.gch
--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "codegen.h"
+#include "type.h"
+#include "tokenizer.h"
+#include "parser.h"
 
 char *read_file(const char *file_path) {
   FILE *fle = fopen(file_path, "r");
@@ -25,4 +29,23 @@ int main(int argc, char **argv) {
     puts("Usage: compiler <file>");
     exit(1);
   }
+
+  filename = argv[1];
+  char* buffer = read_file(filename);
+  inpt = buffer;
+  t = tokenize(buffer);
+  struct program *p = prog();
+  add_type(p); // seg fault here
+  
+  for (struct fun *fn = p->fns; fn != NULL; fn = fn->next) {
+    int ot = 0;
+    for (struct var_list *vl = fn->locals; vl != NULL; vl = vl->next) {
+      struct va *v = vl->v;
+      ot += size_of(v->ty);
+      vl->v->offset = ot;      
+    }
+    fn->stack_size = align_to(ot, 8);    
+  }
+  codegen(p);
+  return 0;  
 }
